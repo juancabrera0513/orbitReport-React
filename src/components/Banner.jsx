5ad390dca92ca8362ba08f084e6083566b8144ff
ZerefRD <juanman.1305@gmail.com>
2024-08-29 17:36:56 -0400

    first commit and all test passed

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./styling.css"
+
+const Banner = () => {
+  return (
+    <>
+    <header>Orbit Report</header>
+    <p>Click on the buttons to see the satellites in that orbit type</p>
+    </>
+  )
+}
+
+export default Banner
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,19 @@
+import "./styling.css"
+import satData from "./satData.jsx";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className="flex-container">
+      {displaySats.map((sat, id) => (
+        <button
+          key={id}
+          onClick={() => filterByType(sat)}
+        >
+          {sat} Orbit
+        </button>
+      ))}
+
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
